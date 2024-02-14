e9a74fafd7b5d747f1da7a4955ff439dee404a1b
SinaVn <vosogh858sina@gmail.com>
2024-02-14 00:54:49 -0800

    Initial Commit

---
+++ instagramHome/instagramHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  instagramHomeApp.swift
+//  instagramHome
+//
+//  Created by Sina Vosough Nia on 11/25/1402 AP.
+//
+
+import SwiftUI
+
+@main
+struct instagramHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
